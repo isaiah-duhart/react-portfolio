c0cdafacddd2fadaacb152368353ed18c82e2141
Isaiah Duhart <isaiah@Isaiahs-MacBook-Pro.local>
2024-12-29 20:20:44 -0500

    Projects now open in new tabs

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function ProjectCard({ src, link, h3, p }) {
   return (
-    <a href={link}>
-      <img className="hover" src={src} alt={`${h3} logo`} target="_blank" />
+    <a href={link} target="_blank">
+      <img className="hover" src={src} alt={`${h3} logo`}/>
       <h3>{h3}</h3>
       <p>{p}</p>
     </a>
