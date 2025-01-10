ce08888eb84dfc3a2c71e4b2416e62768c29b477
Isaiah Duhart <isaiah.duhart.1@gmail.com>
2025-01-09 20:10:12 -0500

    Added attribution for flavour-hub icon

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -4,7 +4,12 @@ function Footer() {
   return (
     <section id="footer" className={styles.container}>
       <p>
-        &copy; 2024 Isaiah Duhart. <br />
+        <a href="https://www.flaticon.com/free-icons/chef" title="chef icons">
+          Chef icons created by Those Icons - Flaticon
+        </a>
+      </p>
+      <p>
+        &copy; 2025 Isaiah Duhart. <br />
         All Rights Reserved
       </p>
     </section>
